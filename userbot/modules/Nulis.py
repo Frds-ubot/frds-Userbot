d719b900116dbaca77cbda9c9edf5674884eb00b
Grey <95908917+grey423@users.noreply.github.com>
2022-04-16 19:20:55 +0700

    Update Nulis.py

--- userbot/modules/Nulis.py
+++ userbot/modules/Nulis.py
@@ -6,12 +6,11 @@ import os
 
 from PIL import Image, ImageDraw, ImageFont
 from userbot.utils import edit_or_reply, edit_delete, text_set
-from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(pattern="nulis(?: |$)(.*)")
+@register(outgoing=True, pattern="^.nulis(?: |$)(.*)")
 async def writer(event):
     if event.reply_to:
         reply = await event.get_reply_message()
@@ -37,36 +36,13 @@ async def writer(event):
     await k.delete()
 
 
-@register(pattern="nulis2(?: |$)(.*)")
-async def writer(event):
-    if event.reply_to:
-        reply = await event.get_reply_message()
-        text = reply.message
-    elif event.pattern_match.group(1).strip():
-        text = event.text.split(maxsplit=1)[1]
-    else:
-        return await edit_delete(event, "Berikan Beberapa Teks")
-    k = await edit_or_reply(event, "Sedang Memproses..")
-    img = Image.open("userbot/resources/buku.jpg")
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("userbot/resources/assfont.ttf", 30)
-    x, y = 150, 140
-    lines = text_set(text)
-    line_height = font.getsize("hg")[1]
-    for line in lines:
-        draw.text((x, y), line, fill=(1, 22, 55), font=font)
-        y = y + line_height - 5
-    file = "grey.jpg"
-    img.save(file)
-    await event.reply(file=file)
-    os.remove(file)
-    await k.delete()
+)
 
 
 CMD_HELP.update(
     {
         "nulis": f"**plugin : **`nulis`\
-        \n\n  •  **syntax :** `{cmd}nulis` <text>\
+        \n\n  •  **syntax :** `.nulis` <text>\
         \n  •  **function : **menulis Teks Di buku ,buat Lu Yang mager nulis\
     "
     }
